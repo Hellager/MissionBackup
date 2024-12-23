83bab6ec521454f0966210597ae655078881514c
Hellager <steinsgu@gmail.com>
2024-12-23 22:52:52 +0800

    feat: add system tray

--- src-tauri/src/core/setup.rs
+++ src-tauri/src/core/setup.rs
@@ -1,10 +1,13 @@
 use tauri::{App, Manager};
 use tauri_plugin_window_state::{WindowExt, StateFlags};
+use crate::core::tray;
 
 pub fn setup_handler(app: &mut App) -> Result<(), Box<dyn std::error::Error + 'static>> {
     let window = app.get_webview_window("main").unwrap();
     let _ = window.restore_state(StateFlags::all());
     log::info!("restore main window state");
+
+    let _ = tray::create_system_tray(app.handle());
 
     Ok(())
 }
